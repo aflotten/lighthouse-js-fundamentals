b8625c83f1995126be40c8c9a52070c5f9c74cd0
Anders Flotten <anders3835@hotmail.com>
2022-08-30 20:47:41 +0000

    Used a for loop to repeat a chorus line

---
+++ chorus-for.js
@@ -0,0 +1,6 @@
+const chorus = "Let's dance!";
+for (let repeat = 0; repeat < 10; repeat++) {//remember sttart;stop;step
+  console.log(chorus);//logs while it evaluates to true in the loop
+}//for loops should be used mostly over while loops
+console.log("Until the sun comes up!");/*logs once as its a global scope and not inside the function*/
+/*important to note that the "repeat" variable can be denoted with simpler denotions such as x and y -> important distinction for simplicity in nested loops*/
