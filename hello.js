1d9fcc803fc7d078f7e5de09b1cf06c2a9109ce1
Anders Flotten <anders3835@hotmail.com>
2022-09-03 03:06:07 +0000

    Added a couple of function examples

---
+++ hello.js
@@ -0,0 +1,13 @@
+// example 1 - 
+const sayHello = function() {
+  console.log("Hello, lad");
+}
+sayHello()
+
+// example 2
+const sayHi = function(name) {
+  console.log("Hello " + name)
+}
+sayHi("Bruce") //needs to be a string or itll pass as undefined
+
+// even number example
