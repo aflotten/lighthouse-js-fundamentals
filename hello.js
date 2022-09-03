826a69a4927248ea7f63c02d658d45d98081edab
Anders Flotten <anders3835@hotmail.com>
2022-09-03 03:12:49 +0000

    Added final function examples

--- hello.js
+++ hello.js
@@ -6,8 +6,19 @@ sayHello()
 
 // example 2
 const sayHi = function(name) {
-  console.log("Hello " + name)
+  console.log("Hello " + name);
 }
 sayHi("Bruce") //needs to be a string or itll pass as undefined
 
-// even number example
+// even number example (return and console log)
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+//return version
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello("John");
+console.log(greeting);
