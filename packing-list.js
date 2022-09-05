2feeedf65e21476519f74221c2a21944413f6405
Anders Flotten <anders3835@hotmail.com>
2022-09-05 19:19:03 +0000

    Added example of for loop through an array

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log("Kitchen stuff to pack:");
+for (let i = 0; i < packingList.length; i++) { //set i to increment through the array by 1 then log it to the console
+  console.log(packingList[i]);
+}
+ 
